0c975aa28bce7661a7f8afd60bec1734ea09d75e
Halima <152661929+Halima-Ablat@users.noreply.github.com>
2024-12-01 20:39:59 +1100

    Add files via upload

--- PasswordGenerator.jsx
+++ PasswordGenerator.jsx
@@ -1,10 +1,36 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function PasswordGenerator() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+";
+
+    for (let i = 1; i < length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
+
+  useEffect(() => {
+    generatePassword();
+  }, [length, numberAllowed, charAllowed]);
+
+  function copyPassword() {
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current.select();
+  }
 
   return (
     <>
@@ -19,9 +45,12 @@ function PasswordGenerator() {
             value={password}
             placeholder="Password"
             readOnly
+            ref={passwordRef}
             className="form-control"
           />
-          <button className="btn btn-primary">copy</button>
+          <button onClick={copyPassword} className="btn btn-primary">
+            copy
+          </button>
         </div>
 
         <div className="d-flex justify-content-center">
